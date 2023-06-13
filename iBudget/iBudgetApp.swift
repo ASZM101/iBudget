53e15e23604e5d9e5c9088db0f90f994c18ceb19
ASZM101 <adrianasofia.zambrano@gmail.com>
2023-06-12 19:21:58 -0500

    Initial Commit

---
+++ iBudget/iBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  iBudgetApp.swift
+//  iBudget
+//
+//  Created by Adriana Zambrano on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct iBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
